1d81a1b1d8cb51bf38b86fddd746238b8b8ca6d2
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-01-18 03:35:32 -0400

    adcionada as actions de outras tables para o site

---
+++ lib/actions/law/law.delete.action.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { withPostAuth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { Law } from "@prisma/client";
+
+export const deleteLaw = withPostAuth(async (_: FormData, law: Law) => {
+  try {
+    const response = await prisma.law.delete({
+      where: {
+        id: law.id,
+      },
+      select: {
+        websiteId: true,
+      },
+    });
+    return response;
+  } catch (error: any) {
+    return {
+      error: error.message,
+    };
+  }
+});
---
+++ lib/actions/poll/poll.update.action.ts
@@ -0,0 +1,66 @@
+"use server"
+
+import { getSession } from "@/lib/auth";
+import { Poll } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { revalidateTag } from "next/cache";
+
+export const updatePoll = async (data: Poll) => {
+  const session = await getSession();
+  if (!session?.user.id) {
+    return {
+      error: "Not authenticatedNão autentificado",
+    };
+  }
+
+  // Verifique se as propriedades essenciais não estão vazias
+  if (!data.question) {
+    return {
+      error: "Título, descrição, e conteúdo não podem estar vazios",
+    };
+  }
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id: data.id,
+    },
+    include: {
+      website: true,
+    },
+  });
+  if (!post || post.userId !== session.user.id) {
+    return {
+      error: "Post não encontrado",
+    };
+  }
+
+  try {
+    const response = await prisma.poll.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        question: data.question,
+      },
+    });
+
+    revalidateTag(
+      `${post.website?.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-posts`,
+    );
+    revalidateTag(
+      `${post.website?.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-${post.slug}`,
+    );
+
+    // if the site has a custom domain, we need to revalidate those tags too
+    post.website?.customDomain &&
+      (revalidateTag(`${post.website?.customDomain}-posts`),
+      revalidateTag(`${post.website?.customDomain}-${post.slug}`));
+
+    return response;
+  } catch (error: any) {
+    return {
+      error: error.message,
+    };
+  }
+};
+
