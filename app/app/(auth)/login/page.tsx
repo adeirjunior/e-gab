04d72feefdae427a400e4b18269c4d12f3537029
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-01-17 23:33:19 -0400

    leis table adicionado

--- app/app/(auth)/login/page.tsx
+++ app/app/(auth)/login/page.tsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
         </Link>
       </p>
 
-      <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full">
+      <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full flex flex-col gap-4">
         <Suspense
           fallback={
             <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
--- app/app/(dashboard)/conteudos/posts/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex max-w-screen-xl mx-auto flex-col space-y-12 p-8">
-      <div className="flex flex-col space-y-6 justify-center items-center">{children}</div>
-    </div>
-  );
-}
