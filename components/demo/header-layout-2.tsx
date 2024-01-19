f61a853cd0b87779c6c5cc2a095959d483d0019d
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-01-18 21:03:44 -0400

    demo nav funcionando

--- components/demo/header-layout-2.tsx
+++ components/demo/header-layout-2.tsx
@@ -1,10 +1,26 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
-import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Dropdown,
+  DropdownTrigger,
+  Button,
+  DropdownMenu,
+  DropdownItem,
+  Accordion,
+  AccordionItem,
+} from "@nextui-org/react";
 import CldImage from "./cloudinary-image";
 import { Mail } from "lucide-react";
-import "./style.css"
+import "./style.css";
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -36,7 +52,7 @@ export default function App() {
       ],
     },
     {
-      name: "utilidades",
+      name: "Utilidades",
       children: [
         {
           name: "Enquetes",
@@ -68,7 +84,8 @@ export default function App() {
 
   return (
     <Navbar
-      className="absolute bg-[#90c9ff1b]"
+      className="absolute bg-transparent"
+      isBlurred={false}
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
     >
@@ -80,82 +97,147 @@ export default function App() {
 
       <NavbarContent className="pr-3 sm:hidden" justify="center">
         <NavbarBrand>
-          <CldImage
-            alt=""
-            width={200}
-            height={100}
-            src="E-Gab/Demo/bqog03j40xuqadomsdfb"
-          />
+          <Link href="/">
+            {" "}
+            <CldImage
+              alt=""
+              width={200}
+              height={100}
+              src="E-Gab/Demo/bqog03j40xuqadomsdfb"
+            />
+          </Link>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden gap-4 sm:flex" justify="center">
         <NavbarBrand>
-          <CldImage
-            alt=""
-            width={200}
-            height={100}
-            src="E-Gab/Demo/bqog03j40xuqadomsdfb"
-          />
+          <Link href="/">
+            <CldImage
+              alt=""
+              width={200}
+              height={100}
+              src="E-Gab/Demo/bqog03j40xuqadomsdfb"
+            />
+          </Link>
         </NavbarBrand>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent justify="end">
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="hidden bg-transparent p-0 data-[hover=true]:bg-transparent sm:block"
+                radius="sm"
+                variant="light"
+              >
+                Parlamentar
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            className="w-[340px]"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem href="/agenda" key="autoscaling">
+              Agenda
+            </DropdownItem>
+            <DropdownItem href="/bibliografia" key="usage_metrics">
+              Bibliografia
+            </DropdownItem>
+            <DropdownItem href="/emendas" key="production_ready">
+              Emendas
+            </DropdownItem>
+            <DropdownItem href="/galeria" key="99_uptime">
+              Galeria
+            </DropdownItem>
+            <DropdownItem href="/projetos" key="supreme_support">
+              Projetos
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="hidden bg-transparent p-0 data-[hover=true]:bg-transparent sm:block"
+                radius="sm"
+                variant="light"
+              >
+                Utilidades
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            className="w-[340px]"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem href="/enquetes" key="autoscaling">
+              Enquetes
+            </DropdownItem>
+            <DropdownItem href="/leis" key="usage_metrics">
+              Leis
+            </DropdownItem>
+            <DropdownItem href="/ouvidoria" key="production_ready">
+              Ouvidoria
+            </DropdownItem>
+            <DropdownItem href="/pesquisas" key="99_uptime">
+              Pesquisas
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarItem className="hidden sm:block">
+          <Link color="foreground" href="/contato">
+            Contato
+          </Link>
+        </NavbarItem>
+        <NavbarItem className="hidden sm:block">
+          <Link color="foreground" href="/posts">
+            Posts
+          </Link>
+        </NavbarItem>
         <Mail color="#fff" />
       </NavbarContent>
 
-      <NavbarMenu className="bg-[#90c9ff1b]">
+      <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             {item.children ? (
               <>
-                <Link
-                  className="w-full"
-                  color={
-                    index === 2
-                      ? "warning"
-                      : index === menuItems.length - 1
-                        ? "danger"
-                        : "foreground"
-                  }
-                  href="#"
-                  size="lg"
-                >
-                  {item.name}
-                </Link>
-                <NavbarMenu>
-                  {item.children.map((child, childIndex) => (
-                    <NavbarMenuItem key={`${child}-${childIndex}`}>
-                      <Link
-                        className="w-full"
-                        color="foreground"
-                        href={`#${child.href}`}
-                        size="lg"
-                      >
-                        {child.name}
-                      </Link>
-                    </NavbarMenuItem>
-                  ))}
-                </NavbarMenu>
+                <NavbarMenuItem>
+                  <Accordion isCompact variant="light">
+                    <AccordionItem
+                      key="1"
+                      aria-label="Accordion 1"
+                      title={item.name}
+                    >
+                        {item.children.map((child, childIndex) => (
+                          <NavbarMenuItem key={`${child}-${childIndex}`}>
+                            <Link
+                              className="w-full"
+                              color="foreground"
+                              href={`#${child.href}`}
+                              size="lg"
+                            >
+                              {child.name}
+                            </Link>
+                          </NavbarMenuItem>
+                        ))}
+                    </AccordionItem>
+                  </Accordion>
+                </NavbarMenuItem>
               </>
             ) : (
               <Link
-                className="w-full"
+                className="w-full px-2"
                 color={
                   index === 2
                     ? "warning"
