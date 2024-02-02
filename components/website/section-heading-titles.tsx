67cd7bb1b22c7e39fc3c7a788517a39493199009
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-02 00:43:18 -0400

    atualidado section title

--- components/website/section-heading-titles.tsx
+++ components/website/section-heading-titles.tsx
@@ -12,10 +12,27 @@ export default function SectionHeadingTitles({
   description,
 }: Props) {
   return (
-    <div className="text-center">
-      <span className="text-sm font-bold text-[#3B5DD9]">{subtitle}</span>
-      <h1 id={id} className="text-xl font-bold">{title}</h1>
-      <p className="hidden sm:block">{description}</p>
+    <div className="mx-auto max-w-3xl text-center">
+      <span className="text-sm font-bold text-[#3B5DD9] sm:text-xl">{subtitle}</span>
+      <h2 id={id} className="text-3xl font-bold text-gray-900 sm:text-4xl">
+        {title}
+      </h2>
+      <p className="mt-4 hidden text-gray-500 sm:block sm:text-xl">
+        {description}
+      </p>
     </div>
   );
 }
+
+   //     <div className="mx-auto max-w-3xl text-center">
+  //        <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+  //          Trusted by eCommerce Businesses
+ //         </h2>
+
+  //        <p className="mt-4 text-gray-500 sm:text-xl">
+ //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+  //          dolores laborum labore provident impedit esse recusandae facere
+ //           libero harum sequi.
+ //         </p>
+ //       </div>;
+
--- components/website/stats-grid.tsx
+++ components/website/stats-grid.tsx
@@ -1,24 +1,23 @@
+import SectionHeadingTitles from "./section-heading-titles"
+
 export default function StatsGrid() {
   return (
     <section className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
-        <div className="mx-auto max-w-3xl text-center">
-          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            Trusted by eCommerce Businesses
-          </h2>
+        <SectionHeadingTitles
+          id="resumo"
+          title="Um resumo de todo meu trabalho"
+          subtitle="Veja"
+          description=""
+        />
 
-          <p className="mt-4 text-gray-500 sm:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-            dolores laborum labore provident impedit esse recusandae facere
-            libero harum sequi.
-          </p>
-        </div>
+        
 
         <div className="mt-8 sm:mt-12">
           <dl className="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:divide-x sm:divide-gray-100">
             <div className="flex flex-col px-4 py-8 text-center">
               <dt className="order-last text-lg font-medium text-gray-500">
-                Total Sales
+                Leis aprovadas
               </dt>
 
               <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
@@ -28,7 +27,7 @@ export default function StatsGrid() {
 
             <div className="flex flex-col px-4 py-8 text-center">
               <dt className="order-last text-lg font-medium text-gray-500">
-                Official Addons
+                Indicações legislativas
               </dt>
 
               <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
@@ -38,7 +37,7 @@ export default function StatsGrid() {
 
             <div className="flex flex-col px-4 py-8 text-center">
               <dt className="order-last text-lg font-medium text-gray-500">
-                Total Addons
+                Moções
               </dt>
 
               <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
