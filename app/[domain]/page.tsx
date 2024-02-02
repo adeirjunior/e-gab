5b7156f7fbeab6fb99801194e2be718e5a4fa847
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-01 21:32:56 -0400

    Update page.tsx

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -55,28 +55,9 @@ export default async function SiteHomePage({
   return (
     <>
       <div className="mb-20 w-full">
-        {posts.length > 0 ? (<Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2"> {posts.map((post, index) => <PostCard key={index} post={post}/>)}</Grid>) 
-         : (
-          <div className="flex flex-col items-center justify-center py-20">
-            <Image
-              alt="missing post"
-              src="https://illustrations.popsy.co/gray/success.svg"
-              width={400}
-              height={400}
-              className="dark:hidden"
-            />
-            <Image
-              alt="missing post"
-              src="https://illustrations.popsy.co/white/success.svg"
-              width={400}
-              height={400}
-              className="hidden dark:block"
-            />
-            <p className="font-title text-2xl text-stone-600 dark:text-stone-400">
-              Sem posts ainda.
-            </p>
-          </div>
-        )}
+        <Banner />
+        <StatsGrid />
+        
       </div>
 
       {proposals.length >= 1 && (
