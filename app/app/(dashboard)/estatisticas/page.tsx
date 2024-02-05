6e1d84d91c28984218a6c645a1f5e0f3fc2c8e4e
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-05 11:55:39 -0400

    onboarding component added

--- app/app/(dashboard)/estatisticas/page.tsx
+++ app/app/(dashboard)/estatisticas/page.tsx
@@ -1,16 +1,16 @@
-import { createCustomerIfNull, hasSubscription } from "@/lib/helpers/billing";
-import Button from "./button";
-import { getSession } from "@/lib/auth/get-session";
+import { hasSubscription } from "@/lib/helpers/billing";
+import OnboardingExample from "@/components/onboarding";
+import ButtonC from "./button";
 
 export default async function page() {
-  const session = await getSession()
   const hasSub = await hasSubscription()
   return (
     <div className="p-8">
       <h1 className="m-0 mb-2 font-cal text-xl font-bold sm:text-3xl dark:text-white">
         Estatísticas
       </h1>
-      <Button hasSub={hasSub} />
+      <OnboardingExample />
+      <ButtonC hasSub={hasSub} />
     </div>
   );
 }
