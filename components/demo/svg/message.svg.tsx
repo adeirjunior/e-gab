9678eec7ea597b00d6543b6a304fa7cb21ceddb7
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-06 14:51:47 -0400

    adicionado svgs

---
+++ components/demo/svg/message.svg.tsx
@@ -0,0 +1,16 @@
+
+export default function Page() {
+  return (
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clipPath="url(#clip0_221_440)">
+<path d="M33.3333 3.32593H6.66668C4.83334 3.32593 3.35001 4.82593 3.35001 6.65926L3.33334 36.6593L10 29.9926H33.3333C35.1667 29.9926 36.6667 28.4926 36.6667 26.6593V6.65926C36.6667 4.82593 35.1667 3.32593 33.3333 3.32593ZM30 23.3259H10V19.9926H30V23.3259ZM30 18.3259H10V14.9926H30V18.3259ZM30 13.3259H10V9.99259H30V13.3259Z" fill="#3B5DD9"/>
+</g>
+<defs>
+<clipPath id="clip0_221_440">
+<rect width="40" height="40" fill="white" transform="translate(0 -0.00732422)"/>
+</clipPath>
+</defs>
+</svg>
+
+  );
+}
