2f72d0b1e04b4826a92df49f28f7c8b767ac8e24
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-07 10:46:22 -0400

    fix

--- components/MessageField.tsx
+++ components/MessageField.tsx
@@ -1,6 +1,5 @@
 import { createMessage } from "@/lib/actions/message/message.create.action";
 import { Button, Card, Input } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 interface MessageFieldProps {
