6b11687890abcfb19b1f5043181c4d4639236c6a
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-14 21:06:44 -0400

    Update room.ts

--- lib/fetchers/room.ts
+++ lib/fetchers/room.ts
@@ -1,26 +1,21 @@
 "use server"
 
-import { unstable_cache } from "next/cache";
-
 export async function getRooms(websiteId: string) {
-  if (!websiteId) {
-    // Trate o caso em que websiteId é undefined
-    console.error("O parâmetro 'websiteId' não pode ser undefined.");
-    return null; // ou lançar uma exceção, dependendo do seu caso
-  }
-
-  return await unstable_cache(
-    async () => {
-      return await prisma.chatRoom.findMany({
+    try {
+      const rooms = await prisma.chatRoom.findMany({
         where: {
           websiteId,
         },
       });
-    },
-    [`${websiteId}-metadata`],
-    {
-      revalidate: 900,
-      tags: [`${websiteId}-metadata`],
-    },
-  )();
+      if (!rooms) {
+        console.log("Website não encontrado.");
+        return null;
+      }
+      return rooms;
+    } catch (error) {
+      console.error("Erro ao buscar o site:", error);
+      throw error;
+    } finally {
+      await prisma.$disconnect();
+    }
 }
