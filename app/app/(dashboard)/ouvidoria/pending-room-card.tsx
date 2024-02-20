68b924319db87319c97f0463293be06321b97a7b
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-02-20 10:39:54 -0400

    modal de rejeitar sala de ouvidoria

--- app/app/(dashboard)/ouvidoria/pending-room-card.tsx
+++ app/app/(dashboard)/ouvidoria/pending-room-card.tsx
@@ -6,6 +6,12 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
 } from "@nextui-org/react";
 import { Bold, Text, Title } from "@tremor/react";
 import ViewModal from "./view-modal";
@@ -24,23 +30,57 @@ export default function PendingRoomCard({
   id: string;
 }) {
   const [isPending, start] = useTransition();
-  const router = useRouter()
+  const router = useRouter();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const updateAccept = (status: ChatRoomStatus) => {
     const formData = new FormData();
     formData.append("status", status);
     start(async () => {
       const room = await updateChatRoom(formData, id, "status");
-      if("error" in room) {
-        toast.error(room.error)
+      if ("error" in room) {
+        toast.error(room.error);
       } else {
-        router.refresh()
+        router.refresh();
       }
     });
   };
 
   return (
     <div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Tem certeza?
+              </ModalHeader>
+              <ModalBody>
+                <Text className="text-gray-300">
+                  Ao rejeitar este contato você poderá estar recusando de ouvir
+                  as reclamações de um eleitor
+                </Text>
+              </ModalBody>
+              <ModalFooter>
+                <form action={() => updateAccept("denied")}>
+                  <Button
+                    type="submit"
+                    color="danger"
+                    variant="light"
+                    onPress={onClose}
+                  >
+                    Confirmar
+                  </Button>
+                </form>
+
+                <Button color="primary" onPress={onClose}>
+                  Cancelar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <span className="relative z-40 float-end -mb-3 -ml-3 flex h-4 w-4">
         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
         <span className="relative inline-flex h-4 w-4 rounded-full bg-sky-500"></span>
@@ -50,10 +90,7 @@ export default function PendingRoomCard({
           <div className="flex flex-col">
             <Title>
               Sala de{" "}
-              <Bold>
-                {room.client.user?.name ||
-                  room.client.user?.email}
-              </Bold>
+              <Bold>{room.client.user?.name || room.client.user?.email}</Bold>
             </Title>
             <Text className="dark:text-gray-400">{room.title}</Text>
           </div>
@@ -74,11 +111,16 @@ export default function PendingRoomCard({
                   {isPending ? "" : "Aceitar"}
                 </Button>
               </form>
-              <form action={() => updateAccept("denied")}>
-                <Button type="submit" color="danger">
-                  Rejeitar
-                </Button>
-              </form>
+
+              <Button
+                spinner={<LoadingDots color="#808080" />}
+                isLoading={isPending}
+                type="submit"
+                onPress={onOpen}
+                color="danger"
+              >
+                {isPending ? "" : "Rejeitar"}
+              </Button>
             </div>
             <ViewModal title={room.title} description={room.description} />
           </div>
