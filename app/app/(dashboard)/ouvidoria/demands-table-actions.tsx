f03d0beed77cdc68b45e986a5466cf26671410e5
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-03-06 13:36:40 -0400

    atualizada ações na table de dependencias

--- app/app/(dashboard)/ouvidoria/demands-table-actions.tsx
+++ app/app/(dashboard)/ouvidoria/demands-table-actions.tsx
@@ -1,26 +1,57 @@
-"use client"
+"use client";
 
 import { VerticalDotsIcon } from "@/components/icons/VerticalDotsIcon";
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Tooltip,
+  useDisclosure,
+} from "@nextui-org/react";
 import { EditIcon, EyeIcon } from "lucide-react";
 import { DeleteDocumentIcon } from "@/components/icons/DeleteDocumentIcon";
 import { Bold, Text, Title } from "@tremor/react";
 import BlurImage from "@/components/arquives/blur-image";
 
-
 export default function DemandsTableActions({
   demandsFormatted,
-  isFullscreen
+  isFullscreen,
 }: {
   demandsFormatted: any;
-  isFullscreen
-: boolean}) {
-const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  isFullscreen: boolean;
+}) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <>
       {isFullscreen ? (
-        <div>dadasdas</div>
+        <>
+          <Tooltip content="Ver detalhes">
+            <span
+              onClick={onOpen}
+              className="cursor-pointer text-lg text-default-400 active:opacity-50"
+            >
+              <EyeIcon />
+            </span>
+          </Tooltip>
+          <Tooltip content="Editar">
+            <span className="cursor-pointer text-lg text-default-400 active:opacity-50">
+              <EditIcon />
+            </span>
+          </Tooltip>
+          <Tooltip color="danger" content="Deletar">
+            <span className="cursor-pointer text-lg text-danger active:opacity-50">
+              <DeleteDocumentIcon />
+            </span>
+          </Tooltip>
+        </>
       ) : (
         <Dropdown>
           <DropdownTrigger>
@@ -64,15 +95,17 @@ const { isOpen, onOpen, onOpenChange } = useDisclosure();
                 <Text className="dark:text-gray-400">
                   {demandsFormatted.description}
                 </Text>
-                {demandsFormatted.startingFiles.map((file: any, index: number) => (
-                  <BlurImage
-                    key={index}
-                    width={300}
-                    height={600}
-                    alt=""
-                    src={file}
-                  />
-                ))}
+                {demandsFormatted.startingFiles.map(
+                  (file: any, index: number) => (
+                    <BlurImage
+                      key={index}
+                      width={300}
+                      height={600}
+                      alt=""
+                      src={file}
+                    />
+                  ),
+                )}
               </ModalBody>
               <ModalFooter>
                 <Button onPress={onClose} variant="light">
