146fab580c9a3d5151713ec4f49368eb02458cb9
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-03-06 08:07:42 -0400

    Update image.create.action.ts

--- lib/actions/image/image.create.action.ts
+++ lib/actions/image/image.create.action.ts
@@ -20,60 +20,59 @@ export async function create(
   key: string,
   tags?: string[],
 ) {
-  const session = await getSession();
-
-  if (!session) {
-    throw new Error("Erro");
-  }
-
-  const file = formData.get(key) as File;
-
-  const imageReader = file.stream().getReader();
-  const imageDataU8: any[] = [];
-
-  while (true) {
-    const { done, value } = await imageReader.read();
-    if (done) break;
-
-    imageDataU8.push(...value);
-  }
-
-  const imageBinary = Buffer.from(imageDataU8 as any, "binary");
-
   try {
+    const session = await getSession();
+
+    if (!session) {
+      throw new Error("Erro");
+    }
+
+    const file = formData.get(key) as File;
+
+    const imageReader = file.stream().getReader();
+    const imageDataU8: any[] = [];
+
+    while (true) {
+      const { done, value } = await imageReader.read();
+      if (done) break;
+
+      imageDataU8.push(...value);
+    }
+
+    const imageBinary = Buffer.from(imageDataU8 as any, "binary");
+
     const filename = randomUUID();
 
     const upload = async () =>
       await new Promise((resolve, reject) => {
-        cloudinary.v2.uploader
-          .upload_stream(
-            {
-              tags: tags ? [...tags] : [],
-              folder: folderPath,
-              public_id: filename,
-              unique_filename: true,
-              discard_original_filename: true,
-            },
-            function (error: any, result: unknown) {
-              if (error) {
-                reject(JSON.stringify(error));
-                return;
-              }
-              resolve(result);
-            },
-          )
-          .end(imageBinary);
+        cloudinary.v2.uploader.upload_stream(
+          {
+            tags: tags ? [...tags] : [],
+            folder: folderPath,
+            public_id: filename,
+            unique_filename: true,
+            discard_original_filename: true,
+          },
+          function (error: any, result: unknown) {
+            if (error) {
+              reject(JSON.stringify(error));
+              return;
+            }
+            resolve(result);
+          },
+        ).end(imageBinary);
       });
 
-       const { folderPath, filePath } = await handleSubmit(
-         session.user.id,
-         filename,
-       );
+    const { folderPath, filePath } = await handleSubmit(session.user.id, filename);
 
-       await upload();
-       return filePath;
-  } catch (error) {}
+    await upload();
+    return filePath;
+  } catch (error) {
+    console.error("Erro na função 'create':", error);
+    throw error;
+  }
 }
+
 
 function excludeCommonPath(basePath: string, excludePath: string): string {
   const remainingPath = excludePath.substring(basePath.length);
