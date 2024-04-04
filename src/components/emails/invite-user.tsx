e61228f68f84edd00d9b918d4c22e0f13a7629af
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-04-04 19:06:56 -0400

    redesign dos emails

--- src/components/emails/invite-user.tsx
+++ src/components/emails/invite-user.tsx
@@ -1,30 +1,22 @@
+import { getCurrentDomain } from "@/lib/utils";
 import {
   Body,
   Container,
-  Column,
   Head,
   Heading,
   Html,
   Img,
   Link,
   Preview,
-  Row,
   Section,
   Text,
 } from "@react-email/components";
-import * as React from "react";
 
 interface SlackConfirmEmailProps {
   validationCode?: string;
 }
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "";
-
-export const InviteUserEmail = ({
-  validationCode,
-}: SlackConfirmEmailProps) => (
+export const InviteUserEmail = ({ validationCode }: SlackConfirmEmailProps) => (
   <Html>
     <Head />
     <Preview>Confirm your email address</Preview>
@@ -32,16 +24,16 @@ export const InviteUserEmail = ({
       <Container style={container}>
         <Section style={logoContainer}>
           <Img
-            src={`${baseUrl}/static/slack-logo.png`}
+            src={getCurrentDomain("", "/logo.png")}
             width="120"
-            height="36"
-            alt="Slack"
+            height="120"
+            alt="Logo do E-Gab"
           />
         </Section>
-        <Heading style={h1}>Confirm your email address</Heading>
+        <Heading style={h1}>Você foi convidado</Heading>
         <Text style={heroText}>
-          Your confirmation code is below - enter it in your open browser window
-          and we&apos;ll help you get signed in.
+          Crie sua conta no <Link href={getCurrentDomain()}>E-Gab</Link> e se
+          coloque como convidado, então ponha o código abaixo.
         </Text>
 
         <Section style={codeBox}>
@@ -49,142 +41,19 @@ export const InviteUserEmail = ({
         </Section>
 
         <Text style={text}>
-          If you didn&apos;t request this email, there&apos;s nothing to worry about, you
-          can safely ignore it.
+          Não conhece quem te convidou? não se preocupe, simplesmente ignore
+          este email.
         </Text>
-
-        <Section>
-          <Row style={footerLogos}>
-            <Column style={{ width: "66%" }}>
-              <Img
-                src={`${baseUrl}/static/slack-logo.png`}
-                width="120"
-                height="36"
-                alt="Slack"
-              />
-            </Column>
-            <Column>
-              <Section>
-                <Row>
-                  <Column>
-                    <Link href="/">
-                      <Img
-                        src={`${baseUrl}/static/slack-twitter.png`}
-                        width="32"
-                        height="32"
-                        alt="Slack"
-                        style={socialMediaIcon}
-                      />
-                    </Link>
-                  </Column>
-                  <Column>
-                    <Link href="/">
-                      <Img
-                        src={`${baseUrl}/static/slack-facebook.png`}
-                        width="32"
-                        height="32"
-                        alt="Slack"
-                        style={socialMediaIcon}
-                      />
-                    </Link>
-                  </Column>
-                  <Column>
-                    <Link href="/">
-                      <Img
-                        src={`${baseUrl}/static/slack-linkedin.png`}
-                        width="32"
-                        height="32"
-                        alt="Slack"
-                        style={socialMediaIcon}
-                      />
-                    </Link>
-                  </Column>
-                </Row>
-              </Section>
-            </Column>
-          </Row>
-        </Section>
-
-        <Section>
-          <Link
-            style={footerLink}
-            href="https://slackhq.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Our blog
-          </Link>
-          &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-          <Link
-            style={footerLink}
-            href="https://slack.com/legal"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Policies
-          </Link>
-          &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-          <Link
-            style={footerLink}
-            href="https://slack.com/help"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Help center
-          </Link>
-          &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-          <Link
-            style={footerLink}
-            href="https://slack.com/community"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-auth="NotApplicable"
-            data-linkindex="6"
-          >
-            Slack Community
-          </Link>
-          <Text style={footerText}>
-            ©2022 Slack Technologies, LLC, a Salesforce company. <br />
-            500 Howard Street, San Francisco, CA 94105, USA <br />
-            <br />
-            All rights reserved.
-          </Text>
-        </Section>
       </Container>
     </Body>
   </Html>
 );
 
 InviteUserEmail.PreviewProps = {
-  validationCode: "DJZ-TLX",
+  validationCode: "8fb2a405-503e-4344-8543-6e8d93f4c9ee",
 } as SlackConfirmEmailProps;
 
 export default InviteUserEmail;
-
-const footerText = {
-  fontSize: "12px",
-  color: "#b7b7b7",
-  lineHeight: "15px",
-  textAlign: "left" as const,
-  marginBottom: "50px",
-};
-
-const footerLink = {
-  color: "#b7b7b7",
-  textDecoration: "underline",
-};
-
-const footerLogos = {
-  marginBottom: "32px",
-  paddingLeft: "8px",
-  paddingRight: "8px",
-  width: "100%",
-};
-
-const socialMediaIcon = {
-  display: "inline",
-  marginLeft: "32px",
-};
 
 const main = {
   backgroundColor: "#ffffff",
