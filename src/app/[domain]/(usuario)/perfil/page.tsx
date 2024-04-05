67f652ade2644d2db8b0ea94f0df4897d9715418
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-04-04 23:41:18 -0400

    Update page.tsx

--- src/app/[domain]/(usuario)/perfil/page.tsx
+++ src/app/[domain]/(usuario)/perfil/page.tsx
@@ -12,32 +12,32 @@ export default async function Page() {
     redirect("/login");
   }
 
-  const user = await getUserById(session.user.id)
+  const user = await getUserById(session.user.id);
 
   return (
     <div className="min-h-screen space-y-4">
       <Card className="min-h-96 w-full rounded-xl bg-gray-600"></Card>
-      <div className="flex gap-4 justify-between ">
-        <div className="flex gap-4">
-           <Card className="-mt-20 ml-6 rounded-full">
-          <ClientProfile alt="Imagem de perfil" src={user.image} />
-        </Card>
-        <h2 className="my-4 text-2xl font-semibold">{user.name}</h2>
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
+        <div className="flex flex-col sm:flex-row gap-4 items-center">
+          <Card className="-mt-20 mx-auto sm:ml-6 rounded-full w-fit h-fit">
+            <ClientProfile alt="Imagem de perfil" src={user.image} />
+          </Card>
+          <h2 className="my-4 text-2xl font-semibold">{user.name}</h2>
         </div>
-       <Card className="max-w-60 border-3 p-4 w-full">
-        <Title>Nível</Title>
-        <p className="text-2xl font-extrabold">{user.client?.level}</p>
-        <Progress
-          label="Exp"
-          showValueLabel
-          color="warning"
-          aria-label="Loading..."
-          value={user.client?.exp}
-          formatOptions={{ style: "decimal"}}
-        />
-      </Card>
-    </div>
+        <Card className="w-full max-w-60 border-3 p-4">
+          <Title>Nível</Title>
+          <p className="text-2xl font-extrabold">{user.client?.level}</p>
+          <Progress
+            label="Exp"
+            showValueLabel
+            color="warning"
+            aria-label="Loading..."
+            value={user.client?.exp}
+            maxValue={user.client?.maxExp}
+            formatOptions={{ style: "decimal" }}
+          />
+        </Card>
       </div>
-      
+    </div>
   );
 }
