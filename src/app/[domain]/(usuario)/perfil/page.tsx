fd201efc3a646ed45b9ef4c6041d85197c61d79f
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-04-04 20:50:18 -0400

    Client Schema melhorado

--- src/app/[domain]/(usuario)/perfil/page.tsx
+++ src/app/[domain]/(usuario)/perfil/page.tsx
@@ -1,3 +1,10 @@
+"use client"
+
+import { CldImage } from "next-cloudinary";
+
 export default function Page() {
-  return <div className="min-h-screen"></div>;
+  return (
+  <div className="min-h-screen">
+<CldImage className="w-full" src="" width={500} height={300} alt="Capa"/>
+  </div>);
 }
