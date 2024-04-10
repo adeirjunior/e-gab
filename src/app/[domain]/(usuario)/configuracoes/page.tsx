2af992054c30a94cda2ce4f063fc6e2463e355ff
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-04-10 15:40:58 -0400

    alterar email, photo e nome usuário do site do político

--- src/app/[domain]/(usuario)/configuracoes/page.tsx
+++ src/app/[domain]/(usuario)/configuracoes/page.tsx
@@ -1,3 +1,53 @@
-export default function Page() {
-  return <div className="min-h-screen"></div>;
+import Form from "@/components/form";
+import { editUser } from "@/lib/actions/user/user.update.action";
+import { getSession } from "@/lib/auth/get-session";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await getSession()
+
+  if(!session) {
+    redirect("/login")
+  }
+
+  return (
+    <div className="min-h-screen space-y-6">
+      <Form
+        title="Nome"
+        description="Seu nome nesta plataforma."
+        helpText="Por favor use no máximo 32 caracteres."
+        inputAttrs={{
+          name: "name",
+          type: "text",
+          defaultValue: session.user.name!,
+          placeholder: "Brendon Urie",
+          maxLength: 32,
+        }}
+        handleSubmit={editUser}
+      />
+      <Form
+        title="Logo"
+        description="A logo do seu perfil. Formatos aceitos: .png, .jpg, .jpeg, .webp"
+        helpText="Arquivo de no máximo 5MB. Tamanho recomendado 400x400."
+        inputAttrs={{
+          name: "logo",
+          type: "file",
+          defaultValue: session.user.image!,
+        }}
+        handleSubmit={editUser}
+      />
+      <Form
+        title="Email"
+        description="Seu email nesta plataforma."
+        helpText="Por favor coloque um email valido."
+        inputAttrs={{
+          name: "email",
+          type: "email",
+          defaultValue: session.user.email!,
+          placeholder: "panic@thedis.co",
+        }}
+        handleSubmit={editUser}
+      />
+    </div>
+  );
 }
