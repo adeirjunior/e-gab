fb9bdbbdba0f0d42b5506dc550b964f396ae5abd
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2024-04-25 13:24:48 -0400

    Update index.ts

--- src/lib/configs/stripe/index.ts
+++ src/lib/configs/stripe/index.ts
@@ -2,7 +2,9 @@ import { isDev } from "@/lib/utils";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(
-   process.env.STRIPE_SECRET_KEY!,
+  !isDev
+    ? process.env.STRIPE_SECRET_KEY_LIVE!
+    : process.env.STRIPE_SECRET_KEY!,
   {
     // https://github.com/stripe/stripe-node#configuration
     apiVersion: "2023-10-16",
