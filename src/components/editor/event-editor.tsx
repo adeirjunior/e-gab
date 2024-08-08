67bc53a539e606696d27687f01c8f24cf48848a9
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-08-08 14:28:38 -0400

    Update event-editor.tsx

--- src/components/editor/event-editor.tsx
+++ src/components/editor/event-editor.tsx
@@ -61,12 +61,12 @@ export default function EventEditor({ event }: { event: EventWithSite }) {
     data.eventLocation.adr_address === event.eventLocation.adr_address &&
     new Date(data.eventStartHour).getTime() ===
       new Date(event.eventStartHour).getTime() &&
-    new Date(data.eventEndHour).getTime() ===
-      new Date(event.eventEndHour).getTime() &&
+    new Date(data.eventEndHour!).getTime() ===
+      new Date(event.eventEndHour!).getTime() &&
     new Date(data.eventStartDay).getTime() ===
       new Date(event.eventStartDay).getTime() &&
-    new Date(data.eventEndDay).getTime() ===
-      new Date(event.eventEndDay).getTime();
+    new Date(data.eventEndDay!).getTime() ===
+      new Date(event.eventEndDay!).getTime();
 
   const updateEventData = async () => {
     const response = await updateEvent(data);
