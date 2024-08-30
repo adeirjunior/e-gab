f56350671fb558addab28216602b0d716d073f64
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2024-08-30 13:52:28 -0400

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from "./providers";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import GoogleAnalytics from "@/components/google-analytics";
 
 const title = "E-Gab - Gabinete Virtual";
 const description = "E-Gab é a maior plataforma para políticos do Brasil, com ela você consegue todas as ferramentas essenciais para o seu trabalho como vereador/deputado em um só lugar!";
@@ -35,6 +36,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
+      <GoogleAnalytics />
       <body className={cn(raleway.variable, cal.variable, inter.variable)}>
         <Providers>
           {children}
